2a223a74fd911cc8d014e3edc97bf6b9d3efeafc
LibereCode <libere-code@posteo.org>
2026-08-26 23:09:01 +0200

    feat: logo = "" -> logos = {};
    fix: cfg.logos
    
    fix: cfg.logos
    
    fix: homeModules

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -1,88 +1,119 @@
 { inputs, self, ... }:
 {
-  flake.homeModules.default =
-    per@{
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    let
-      cfg = config.programs.fetchit;
-      inherit (pkgs.stdenv.hostPlatform) system;
-      pkg = self.packages.${system}.fetchit;
-    in
-    {
-      options =
-        let
-          inherit (lib)
-            types
-            mkOption
-            mkEnableOption
-            ;
-        in
-        {
-          programs.fetchit = {
-            enable = mkEnableOption "fetchit";
+  flake.homeModules = {
+    fetchit =
+      per@{
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      let
+        cfg = config.programs.fetchit;
+        inherit (pkgs.stdenv.hostPlatform) system;
+        pkg = self.packages.${system}.fetchit;
+      in
+      {
+        options =
+          let
+            inherit (lib)
+              types
+              mkOption
+              mkEnableOption
+              ;
+          in
+          {
+            programs.fetchit = {
+              enable = mkEnableOption "fetchit";
 
-            package = mkOption {
-              type = types.package;
-              default = pkg;
-              description = lib.literalMD "`fetchit` **package** to use.";
+              package = mkOption {
+                type = types.package;
+                default = pkg;
+                description = lib.literalMD "`fetchit` **package** to use.";
+              };
+
+              # TODO ?
+              # settings = mkOption {
+              # };
+
+              initLua = lib.mkOption {
+                type = types.nullOr types.str;
+                default = null;
+                description = lib.literalMD ''
+                  init.lua as a stringblock.
+                  If you want to source an external file, simply use:
+                  ```nix
+                  initLua = builtins.readFile ./init.lua;
+                  ```
+                '';
+              };
+
+              # #FIXME: attrs set of { logo_path = logo_text; ... };
+              # logo = lib.mkOption {
+              #   type = types.nullOr types.str;
+              #   default = null;
+              #   description = lib.literalMD ''
+              #     logo.txt as a stringblock.
+              #     If you want to source an external file, simply use:
+              #     ```nix
+              #     logo = builtins.readFile ./logo.txt;
+              #     ```
+              #   '';
+              # };
+
+              logos = lib.mkOption {
+                type = types.nullOr (types.attrsOf types.str);
+                default = null;
+                description = lib.literalMD ''
+                  logo.txt as a stringblock.
+                  If you want to source an external file, simply use:
+                  ```nix
+                  logos = { "logos/logo.txt" = builtins.readFile ./logo.txt; };
+                  ```
+                '';
+                example = {
+                  "logos/logo.txt" = ''
+                    *------*
+                    | 0  0 |
+                    |  --  |
+                    *------*
+                  '';
+                  logo2 = ''
+                     /\_/\ Schröd?
+                    ( X_* )..   ((
+                    >= ^ <=  _))
+                    (__x__)-(__|-'
+                  '';
+                };
+              };
             };
 
-            # TODO ?
-            # settings = mkOption {
-            # };
-
-            initLua = lib.mkOption {
-              type = types.nullOr types.str;
-              default = null;
-              description = lib.literalMD ''
-                init.lua as a stringblock.
-                If you want to source an external file, simply use:
-                ```nix
-                initLua = builtins.readFile ./init.lua;
-                ```
-              '';
-            };
-
-            #TODO: attrs set of { logo_path = logo_text; ... };
-            logo = lib.mkOption {
-              type = types.nullOr types.str;
-              default = null;
-              description = lib.literalMD ''
-                logo.txt as a stringblock.
-                If you want to source an external file, simply use:
-                ```nix
-                logo = builtins.readFile ./logo.txt;
-                ```
-              '';
-            };
           };
+        config = lib.mkIf cfg.enable {
+          home.packages = [ pkg ];
 
+          xdg.configFile = {
+            "fetchit/init.lua" =
+              if cfg.initLua != null then
+                {
+                  text = cfg.initLua;
+                }
+              else
+                {
+                  source = "${cfg.package}/share/pkgit/config/init.lua";
+                };
+
+          }
+          // (
+            if cfg.logos != null then
+              lib.mapAttrs' (name: value: lib.nameValuePair ("fetchit/" + name) { text = value; }) cfg.logos
+            else
+              {
+                "fetchit/logos/logo.txt".source = "${cfg.package}/share/pkgit/config/logos/logo.txt";
+              }
+          );
         };
-      config = lib.mkIf cfg.enable {
-        home.packages = [ pkg ];
-
-        xdg.configFile."fetchit/init.lua" =
-          if cfg.initLua != null then
-            {
-              text = cfg.initLua;
-            }
-          else
-            {
-              source = "${cfg.package}/share/pkgit/config/init.lua";
-            };
-        xdg.configFile."fetchit/logos/logo.txt" =
-          if cfg.logo != null then
-            {
-              text = cfg.logo;
-            }
-          else
-            {
-              source = "${cfg.package}/share/pkgit/config/logos/logo.txt";
-            };
       };
-    };
+    default = self.homeModules.fetchit;
+  };
 }
